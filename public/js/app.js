19136e0881156cd142dd0c8bca73dfa215320336
rohit <kumarrohit00294@gmail.com>
2020-06-30 00:30:56 +0530

    fixes

--- public/js/app.js
+++ public/js/app.js
@@ -4,27 +4,26 @@ const weatherForm = document.querySelector('form');
 const search = document.querySelector('input');
 const messageOne = document.querySelector('#message-1');
 const messageTwo = document.querySelector('#message-2');
-if(!weatherForm){
-    return;
+if(weatherForm){
+    weatherForm.addEventListener('submit', (e)=>{
+        e.preventDefault();
+        const location = search.value;
+        
+        messageOne.textContent = 'Loading...'
+        messageTwo.textContent = ''
+        
+        fetch('/weather?address='+location).then((response)=>{
+            response.json().then((data)=>{
+                if(data.error){
+                    messageOne.textContent = data.error;
+                }else{
+                    messageOne.textContent = data.location;
+                    messageTwo.textContent = data.forecast;
+                }
+            })
+        })
+    
+        console.log(location);
+    })
 }
 
-weatherForm.addEventListener('submit', (e)=>{
-    e.preventDefault();
-    const location = search.value;
-    
-    messageOne.textContent = 'Loading...'
-    messageTwo.textContent = ''
-    
-    fetch('/weather?address='+location).then((response)=>{
-        response.json().then((data)=>{
-            if(data.error){
-                messageOne.textContent = data.error;
-            }else{
-                messageOne.textContent = data.location;
-                messageTwo.textContent = data.forecast;
-            }
-        })
-    })
-
-    console.log(location);
-})
